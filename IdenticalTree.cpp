0af51d6fca6201bc1c85786a9e4d9a9e037aa797
Divyansh Jain <159652110+Divyansh2510@users.noreply.github.com>
2025-12-29 23:53:42 +0530

    Add files via upload

---
+++ IdenticalTree.cpp
@@ -0,0 +1,54 @@
+//Here wew will be checking whetther two trees are identical or not .
+
+#include <iostream>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) {
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL || q == NULL) {
+            return p == q;
+        }
+
+        bool isLeftSame = isSameTree(p->left, q->left);
+        bool isRightSame = isSameTree(p->right, q->right);
+
+        return isLeftSame && isRightSame && p->val == q->val;
+    }
+};
+
+int main() {
+    
+
+    TreeNode* p = new TreeNode(1);
+    p->left = new TreeNode(2);
+    p->right = new TreeNode(3);
+
+    TreeNode* q = new TreeNode(1);
+    q->left = new TreeNode(2);
+    q->right = new TreeNode(3);
+
+    Solution obj;
+    bool result = obj.isSameTree(p, q);
+
+    if (result)
+        cout << "Trees are same" << endl;
+    else
+        cout << "Trees are NOT same" << endl;
+
+    return 0;
+}
+
