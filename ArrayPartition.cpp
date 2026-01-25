4686090dbdef8150ce8e3fec72cf659b9a20656b
Divyansh Jain <159652110+Divyansh2510@users.noreply.github.com>
2026-01-25 23:23:41 +0530

    Add files via upload

---
+++ ArrayPartition.cpp
@@ -0,0 +1,21 @@
+//Array partition problem
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+ int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i += 2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+    
+    
+int main(){
+	vector<int> n = {1,4,3,2};
+	cout<<arrayPairSum(n);
+	return 0;
+}
