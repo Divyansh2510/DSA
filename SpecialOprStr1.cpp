e8ce305531f967d551005d9fdf85adc2b83629c8
Divyansh Jain <159652110+Divyansh2510@users.noreply.github.com>
2026-06-17 11:11:50 +0530

    Add files via upload

---
+++ SpecialOprStr1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<String>
+#include<algorithm>
+using namespace std;
+
+string processStr(string s) {
+         string result = "";
+
+        for (char ch : s) {
+            if (ch >= 'a' && ch <= 'z') {
+                result.push_back(ch);
+            }
+            else if (ch == '*') {
+                if (!result.empty()) {
+                    result.pop_back();
+                }
+            }
+            else if (ch == '#') {
+                result += result;
+            }
+            else if (ch == '%') {
+                reverse(result.begin(), result.end());
+            }
+        }
+
+        return result;
+    }
+    
+int main(){
+	string s = "a#b%*";
+	cout<<processStr(s);
+	return 0;
+}
